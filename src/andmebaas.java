73495d5da6064269b0bcf773778fddafa2e57b41
Madis Võrklaev <Madis Vrklaev>
2016-10-27 00:09:10 +0300

    Graafilise kasutajaliidesega versioon Projektist. Miskipärast ei tööta teisest klassist väljakutsumise meetodid. Teha drop-down menüü sisu lugemine ArrayListist.

---
+++ src/andmebaas.java
@@ -0,0 +1,134 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.*;
+
+public class andmebaas {
+    String name;
+    double power = 0;
+    double voolutarve = 0;
+    double voolKokku = 0;
+    int channels = 0;
+    int dmx = 0;
+    ArrayList tootjaAndmebaasist = new ArrayList();
+    ArrayList nimiAndmebaasist = new ArrayList();
+
+
+    // Andmebaasi loomine
+    public void createDatabase() {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:devices.db");
+            System.out.println("Andmebaas loodud.");
+
+            // Tabeli loomine
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE DEVICES " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " POWER            DOUBLE     NOT NULL, " +
+                    " CHANNELS        INT)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Tabel loodud.");
+
+    }
+
+    // Tekstifaili lugemine
+    private Scanner x;
+
+    public void openFile() {
+        try {
+            x = new Scanner(new File("data.txt"));
+        } catch (Exception e) {
+            System.out.println("Faili ei leitud");
+        }
+    }
+
+    public void readFile() {
+        boolean tableExists = false;
+        while (x.hasNext()) {
+            String a = x.next();
+            String b = x.next();
+            String d = x.next();
+            String e = x.next();
+            String value = "VALUES (" + a + ", '" + b + "', " + d + ", " + e + " );";
+            sql(value);
+
+        }
+    }
+
+    public void closeFile() {
+        x.close();
+    }
+
+    //Faili andmed tabelisse
+    public void sql(String value) {
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:devices.db");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:devices.db");
+            Statement stmt = null;
+            c.setAutoCommit(false);
+            //  System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+
+            String sql = "INSERT INTO DEVICES (ID,NAME,POWER,CHANNELS) " +
+                    value;
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
+    }
+
+    // Seadmete nimed drop-down menüüsse
+    public void getSeadmed() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection c = null;
+            c = DriverManager.getConnection("jdbc:sqlite:devices.db");
+            c.setAutoCommit(false);
+            //  System.out.println("Opened database successfully");
+            Statement stmt = null;
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM DEVICES;");
+            while (rs.next()) {
+                //int id = rs.getInt("id");
+                String name = rs.getString("name");
+                nimiAndmebaasist.add(name);
+               // System.out.println(id + " " + name);
+            }
+            System.out.println(nimiAndmebaasist);
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+}
---
+++ src/kasutajaliides.java
@@ -0,0 +1,86 @@
+/* http://docs.oracle.com/javafx/2/ui_controls/combo-box.htm
+   http://docs.oracle.com/javafx/2/ui_controls/table-view.htm
+ */
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class kasutajaliides extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    andmebaas readObject = new andmebaas();
+        readObject.createDatabase();
+        readObject.openFile();
+        readObject.readFile();
+        readObject.closeFile();
+
+    // Nupud - tekstiväljad
+    final Button button = new Button("Lisa");
+    final TextField vool = new TextField("");
+    final TextField kanaleid = new TextField("");
+    final TextField seadmeid = new TextField("1");
+    private TableView patch = new TableView();
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Java projekt 1.0");
+        Scene scene = new Scene(new Group());
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(700);
+
+        // Drop-down menüüd
+        final ComboBox tootjaComboBox = new ComboBox();
+                tootjaComboBox.getItems().addAll(
+                        "tootjaAndmebaasist1",
+                        "tootjaAndmebaasist2"
+                );
+        final ComboBox seadmeComboBox = new ComboBox();
+                seadmeComboBox.getItems().addAll(
+                         "nimiAndmebaasist1",
+                         "nimiAndmebaasist2"
+        );
+
+        // Tabeli pealkiri
+        final Label label = new Label("Patch list");
+        label.setFont(new Font("Arial", 16));
+
+        // Tabel
+        patch.setEditable(false);
+
+        TableColumn jrk = new TableColumn("Nr");
+        TableColumn seadmeNimi = new TableColumn("Seadme nimi");
+        TableColumn aadress = new TableColumn("Aadress");
+
+        patch.getColumns().addAll(jrk, seadmeNimi, aadress);
+
+        //Layout
+        GridPane grid = new GridPane();
+        grid.setVgap(4);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(5, 5, 5, 5));
+        grid.add(new Label("Vali tootja: "), 0, 0);
+        grid.add(tootjaComboBox, 0, 1);
+        grid.add(new Label("Vali seade: "), 0, 2);
+        grid.add(seadmeComboBox, 0, 3);
+        grid.add(new Label("Mitu seadet: "), 0, 4);
+        grid.add(seadmeid, 0, 5);
+        grid.add(button, 1, 5);
+        grid.add(label, 0, 7);
+        grid.add(patch, 0, 8);
+
+        Group root = (Group)scene.getRoot();
+        root.getChildren().add(grid);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
